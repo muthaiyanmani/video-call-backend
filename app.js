6a657e9bd41e281958188ff99453a777cfe5df5e
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2022-01-16 18:52:06 +0530

    added origin url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "*",
+		origin: "https://video-call-frontend.vercel.app",
 		methods: [ "GET", "POST" ]
 	}
 })
